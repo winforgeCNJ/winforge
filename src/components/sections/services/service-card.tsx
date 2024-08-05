ce128ca61b28fe4d7dc2800eb3c0e457298b3797
Chaya <chaya.karim.omar@gmail.com>
2024-08-05 18:51:33 -0300

    Old Four Services

--- src/components/sections/services/service-card.tsx
+++ src/components/sections/services/service-card.tsx
@@ -1,40 +1,34 @@
-interface ServiceProp {
-  service: {
-    title: string;
-    bold1: string;
-    text1: string;
-    bold2: string;
-    text2: string;
-    footer: string[];
-  };
-  className: string;
+interface ServiceI {
+  title: string;
 }
 
-export default function ServiceCard({
-  service: { title, bold1, text1, bold2, text2, footer },
-  className,
-}: ServiceProp) {
+interface ServiceCardProps {
+  service: ServiceI;
+}
+
+export default function ServiceCard({ service }: ServiceCardProps) {
   return (
-    <article
-      className={`${className} duration-1000 relative h-[24rem] w-[30rem] overflow-hidden rounded-2xl bg-gradient-to-r from-primary via-[#2a3435] to-[#2e2e2ed3] p-1 text-white transition-all`}
-    >
+    <article className=" relative mx-auto h-[26rem] w-auto overflow-hidden rounded-2xl bg-gradient-to-r from-primary via-[#07c7d2] to-[#054494] p-1 text-white lg:h-[28rem] lg:w-[36rem]">
       <div className="flex h-full w-full flex-col items-start justify-between rounded-2xl bg-black px-4 py-8 lg:px-8 lg:py-12">
-        <h3 className="text-lg font-bold uppercase">{title}</h3>
+        <h3 className="text-lg font-bold uppercase">{service.title}</h3>
 
         <div>
-          <h4 className="font-semibold">{bold1}</h4>
-          <p>{text1}</p>
+          <h4 className="font-semibold">Manual de marca:</h4>
+          <p>
+            Logo, Isologo, Isotopo, Nombre, Branding, PLateta de colores
+            institucionales, Tipografías
+          </p>
         </div>
 
         <div>
-          <h4 className="font-semibold">{bold2}</h4>
-          <p>{text2}</p>
+          <h4 className="font-semibold">Espíritu de la marca:</h4>
+          <p>Misión, Visión, Valores, Voz y Tono.</p>
         </div>
 
         <footer className="flex w-full items-center justify-between">
-          {footer.map((item) => (
-            <p className="w-full text-justify">{item}</p>
-          ))}
+          <p>Página web.</p>
+          <p>Eventos.</p>
+          <p>Diseño editorial.</p>
         </footer>
       </div>
     </article>
