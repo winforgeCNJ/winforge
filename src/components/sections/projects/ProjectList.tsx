e84a4169e933ad6fdcd583f42051de0cd7af62a9
Chaya <chaya.karim.omar@gmail.com>
2024-08-15 19:25:11 -0300

    Karim: Update 2

--- src/components/sections/projects/ProjectList.tsx
+++ src/components/sections/projects/ProjectList.tsx
@@ -1,44 +1,101 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import "swiper/css";
-import 'swiper/css/bundle';
-import { useState, useRef } from "react";
+import "swiper/css/bundle";
+import SwiperCore from "swiper";
+import { useState, useRef, useEffect } from "react";
+import { Projects } from "../../../consts/projects.ts";
 
 const ProjectList = () => {
+  const [swiperInstance, setSwiperInstance] = useState<SwiperCore | null>(null);
+  const [swiperWrapper, setSwiperWrapper] = useState<HTMLElement | null>(null);
+  const [centeredSlide, setCenteredSlide] = useState<HTMLElement | null>(null);
+
+  useEffect(() => {
+    const element = document.querySelector(".swiper-wrapper") as HTMLElement;
+    setSwiperWrapper(element);
+    swiperWrapper?.classList.add(
+      "flex",
+      "items-center",
+      "transition-all",
+      "duration-500",
+    );
+
+    const slideInCenter = document.querySelector(
+      ".swiper-slide-active",
+    ) as HTMLElement;
+    setCenteredSlide(slideInCenter);
+    centeredSlide?.classList.add("z-50");
+  });
+
+  const centerSlide = (id: number) => {
+    console.log(swiperInstance?.realIndex);
+
+    swiperInstance?.slideToLoop(id, 1000, true);
+  };
 
   return (
-      <Swiper
-        modules={[Navigation]}
-        navigation={{
-          nextEl: ".card-button-next",
-          prevEl: ".card-button-prev",
-        }}
-        centeredSlides={true}
-        slideActiveClass="scale-110"
-        loop={true}
-        slidesPerView={3}
-        className="relative mt-10 flex w-full items-start justify-center transition-all duration-500"
-      >
-        {[...Array(10)].map((item, index) => (
-          <SwiperSlide key={index} style={{ width: '' }} className="flex overflow-hidden rounded-2xl">
-            <div className="">
-              <img
-                src={"/assets/projects/Cong. Educ. Hum..webp"}
-                className=""
-                alt="placeholder image"
-              />
-            </div>
+    <Swiper
+      onSwiper={(swiper: SwiperCore) => {
+        setSwiperInstance(swiper);
+      }}
+      loop={true}
+      simulateTouch={false}
+      centeredSlides={true}
+      slidesPerGroup={1}
+      slidesPerView={1}
+      watchSlidesProgress={true}
+      speed={1000}
+      breakpoints={{
+        640: {
+          slidesPerView: 3,
+        },
+        1300: {
+          slidesPerView: 5,
+        },
+      }}
+      className="flex h-[80vh] w-full items-center" /*relative mt-10 flex items-center  justify-center transition-all duration-500*/
+    >
+      <div className="swiper-wrapper">
+        {Projects.map((item) => (
+          <SwiperSlide
+            key={item.id}
+            style={{ display: "flex" }}
+            className="h-full items-center"
+          >
+            {({ isActive, isPrev, isNext }) => (
+              <div
+                className={`flex w-full flex-col items-center justify-center transition-all duration-1000 ${isActive || isPrev || isNext ? "" : "opacity-60 blur-[2px]"} ${isPrev ? "mr-12 blur-[0.7px]" : ""} ${isNext ? "ml-12 blur-[0.7px]" : ""}`}
+                key={item.id}
+              >
+                <img
+                  src={item.image}
+                  style={{ width: item.width }}
+                  className={`transition-all duration-1000 ${isActive && (item.name === "Alpha Handel Group" || item.name === "JCB Desarrollos") ? "mb-20 scale-x-[1.7] scale-y-[2]" : isActive ? "mb-20 scale-[2] cursor-pointer" : "mb-4"} ${isPrev ? "mb-10 scale-[1.4] cursor-pointer" : ""} ${isNext ? "mb-10 scale-[1.4] cursor-pointer" : ""} `}
+                  onClick={
+                    isActive || isNext || isPrev
+                      ? () => centerSlide(item.id)
+                      : undefined
+                  }
+                />
+                <p
+                  className={`text-center text-white transition-all duration-1000 ${isActive ? "w-[200px] text-2xl" : ""} ${isPrev ? "w-36 text-xl" : ""} ${isNext ? "w-36 text-xl" : ""} ${isActive || isPrev || isNext ? "" : "w-44"}`}
+                >
+                  {item.name}
+                </p>
+              </div>
+            )}
           </SwiperSlide>
         ))}
-      </Swiper>
-
+      </div>
+    </Swiper>
   );
-}
+};
 
-export default ProjectList
+export default ProjectList;
 
-  
-{/*
+{
+  /*
       <div
         className="flex w-[85vw] rounded-2xl items-center jus"
       >
@@ -55,4 +112,5 @@ export default ProjectList
           </div>
         ))}
       </div>
-      */}
+      */
+}
---
+++ src/consts/projects.ts
@@ -0,0 +1,63 @@
+export const Projects = [
+  {
+    id: 0,
+    name: "Alpha Handel Group",
+    image: "/assets/projects/ALPHA-HANDEL-GROUP.png",
+    width: "150px",
+  },
+  {
+    id: 1,
+    name: "Atenea Incubadora",
+    image: "/assets/projects/Atenea.png",
+    width: "70px",
+  },
+  {
+    id: 2,
+    name: "Ancora Ediciones",
+    image: "/assets/projects/ANCORA.png",
+    width: "95px",
+  },
+  {
+    id: 3,
+    name: "Congreso de Educación Humanista",
+    image: "/assets/projects/Congreso-Educacion-Humanista.png",
+    width: "95px",
+  },
+  {
+    id: 4,
+    name: "Jardin Surcos",
+    image: "/assets/projects/Jardin-Surcos.png",
+    width: "105px",
+  },
+  {
+    id: 5,
+    name: "Disroi Distribuidor",
+    image: "/assets/projects/Disroi.png",
+    width: "110px",
+  },
+  {
+    id: 6,
+    name: "LOTH Clothing",
+    image: "/assets/projects/LOTH.png",
+    width: "75px",
+  },
+  {
+    id: 7,
+    name: "Logos Ediciones",
+    image: "/assets/projects/Logos.png",
+    width: "105px",
+  },
+  {
+    id: 8,
+    name: "JCB Desarrollos",
+    image: "/assets/projects/JCB.png",
+    width: "155px",
+  },
+  {
+    id: 9,
+    name: "La Reina Supermercados",
+    image: "/assets/projects/La-Reina.png",
+    width: "65px",
+  },
+ 
+];
