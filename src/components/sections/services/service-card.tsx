20114175843f0d0e81e35c9101ff9c49caee181f
Chaya <chaya.karim.omar@gmail.com>
2024-08-16 16:09:48 -0300

    Karim: Update 3

--- src/components/sections/services/service-card.tsx
+++ src/components/sections/services/service-card.tsx
@@ -14,18 +14,18 @@ interface ServiceCardProps {
 export default function ServiceCard({ service }: ServiceCardProps) {
   const { title, bold1, text1, bold2, text2, footer } = service;
   return (
-    <article className="relative mx-auto h-[24rem] mobile:h-[26rem] w-auto overflow-hidden rounded-2xl bg-gradient-to-r from-primary via-[#07c7d2] to-[#054494] p-1 text-white lg:h-[28rem] lg:w-[36rem]">
-      <div className="flex h-full w-full flex-col items-start justify-around rounded-2xl bg-black px-4 py-8 sm:justify-between lg:px-8 lg:py-12 [&>div>p]:font-extralight">
+    <article className="relative mx-auto h-[24rem] w-auto overflow-hidden rounded-2xl bg-gradient-to-r from-primary via-[#07c7d2] to-[#054494] p-1 text-white mobile:h-[26rem] lg:h-[28rem] lg:w-[36rem]">
+      <div className="flex h-full w-full flex-col items-start justify-around rounded-2xl bg-black px-4 py-8 sm:justify-between lg:px-8 lg:py-12">
         <h3 className="text-lg font-bold uppercase">{title}</h3>
 
-        <div>
-          <h4 className="font-semibold">{bold1}:</h4>
-          <p>{text1}</p>
+        <div className="max-w-[23rem]">
+          <h4 className="mb-1 font-semibold">{bold1}</h4>
+          <p className="font-extralight max-sm:text-sm">{text1}</p>
         </div>
 
-        <div>
-          <h4 className="font-semibold">{bold2}</h4>
-          <p>{text2}</p>
+        <div className="max-w-[26rem]">
+          <h4 className="mb-1 font-semibold">{bold2}</h4>
+          <p className="font-extralight max-sm:text-sm">{text2}</p>
         </div>
 
         <footer className="hidden w-full items-center justify-between sm:flex">
@@ -34,4 +34,35 @@ export default function ServiceCard({ service }: ServiceCardProps) {
       </div>
     </article>
   );
+}
+
+{
+  /* 
+  
+      <article className="relative mx-auto h-[24rem] w-auto overflow-hidden rounded-2xl bg-gradient-to-r from-primary via-[#07c7d2] to-[#054494] p-1 text-white mobile:h-[26rem] lg:h-[28rem] lg:w-[36rem]">
+      <div className="flex h-full w-full flex-col items-start justify-around rounded-2xl bg-black px-4 py-8 sm:justify-between lg:px-8 lg:py-12 [&>div>p]:font-extralight">
+        <h3 className="text-center text-lg font-bold uppercase w-full">{title}</h3>
+
+        <div className="w-full">
+          <h4 className="mb-3 text-center font-semibold">{bold1}</h4>
+          <div className="flex items-center justify-center">
+            <p className="w-[45%] text-center">{text1a}</p>
+            <p className="w-[45%] text-center">{text1b}</p>
+          </div>
+        </div>
+
+        <div className="w-full">
+          <h4 className="mb-3 text-center font-semibold">{bold2}</h4>
+          <div className="flex items-center justify-center">
+            <p className="w-[45%] text-center">{text2a}</p>
+            <p className="w-[45%] text-center">{text2b}</p>
+          </div>
+        </div>
+
+        <footer className="hidden w-full items-center justify-around sm:flex">
+          {footer?.map((item) => <p>{item}</p>)}
+        </footer>
+      </div>
+    </article>
+  */
 }
--- src/components/sections/services/service-list.tsx
+++ src/components/sections/services/service-list.tsx
@@ -22,7 +22,7 @@ const tabs = [
     content: (
       <ServiceCard
         service={{
-          title: "Desarrollo de Identidada visual y Comunicativa.",
+          title: "Desarrollo de Identidada visual y Comunicativa",
           bold1: "Manual de Marca",
           text1:
             "Logo, Isologo, Isotipo, Naming, Branding, Paleta de Colores Institucionales, Tipografías",
@@ -101,4 +101,98 @@ export function TabsDemo() {
       <Tabs tabs={tabs} />
     </div>
   );
+}
+
+
+{
+  /*
+  const tabs = [
+  {
+    title: <Title title="Diseño y Estrategia" icon={<IconBezier />} />,
+    icon: <IconBezier />,
+    titleOnly: "Diseño",
+    value: "design-and-strategy",
+    content: (
+      <ServiceCard
+        service={{
+          title: "Desarrollo de Identidada visual y Comunicativa",
+          bold1: "Manual de Marca",
+          text1a: "Logo, Isologo, Isotipo, Naming, Branding",
+          text1b: "Paleta de Colores Institucionales, Tipografías",
+          bold2: "Diseño de la Identidad",
+          text2a: "Misión y Visión",
+          text2b: "Valores, Voz y Tono",
+          footer: ["Landing Page", "Eventos", "Diseño Editorial"],
+        }}
+      />
+    ),
+  },
+  {
+    title: <Title title="Desarrollo de Software" icon={<IconWeb />} />,
+    icon: <IconWeb />,
+    titleOnly: "Desarrollo",
+    value: "development-web",
+    content: (
+      <ServiceCard
+        service={{
+          title: "Desarrollo de Software",
+          bold1: "Plataformas Web y Tiendas Online",
+          text1a:,
+          text1b:,
+            "Creación, Diseño y Desarrollo a Medida. Interactivo, Innovador y Responsive.",
+          bold2: "Gestión de Proyectos IT",
+          text2a:
+            "Aplicaciones que permiten a las empresas integrar, automatizar y gestionar sus procesos de manera eficiente y centralizada.",
+          text2b: 
+            footer: [],
+        }}
+      />
+    ),
+  },
+  {
+    title: <Title title="Gestión" icon={<IconEarth />} />,
+    icon: <IconEarth />,
+    titleOnly: "Gestión",
+    value: "playground",
+    content: (
+      <ServiceCard
+        service={{
+          title: "Gestión multimedia",
+          bold1: "Planificación de Contenido en RRSS",
+          text1a:
+            "Instagram, Youtube, Tik Tok, Facebook,  Linkedin, Twitter, Mail",
+          text1b:,
+          bold2: "Posicionamiento adecuado",
+          text2a:
+            "Optimización SEO, Estetica Integral, Marketing Digital, Análisis estratégico de métricas y tendencias.",
+          text2b:,
+          footer: [],
+        }}
+      />
+    ),
+  },
+  {
+    title: <Title title="Producción" icon={<IconFilm />} />,
+    icon: <IconFilm />,
+    titleOnly: "Producción",
+    value: "content",
+    content: (
+      <ServiceCard
+        service={{
+          title: "Producción de contenido audiovisual",
+          bold1: "Brainstorming y Guionización",
+          text1a:
+            "Trabajamos con el cliente para generar ideas y crear contendio acorde a lo que necesiten.",
+          text1b:,
+          bold2: "Grabación y Edición de Calidad",
+          text2a:
+            "Con Softwares avanzados generamos un producto final profesional y de gran impacto.",
+          text2b:,
+          footer: ["Videos Institucionales", "Reels", "Imágenes"],
+        }}
+      />
+    ),
+  },
+];
+  */
 }
